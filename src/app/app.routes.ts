e0f150a892da625c0750fa8e72f24271eea7f3cc
maximilian lamm <maximilianlamm.kontakt@outlook.de>
2024-07-04 16:31:34 +0200

    load courses and filter this courses with signal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,31 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {LessonsComponent} from "./lessons/lessons.component";
-import {isUserAuthenticated} from "./guards/auth.guard";
-import {CourseComponent} from "./course/course.component";
-import {courseResolver} from "./course/course.resolver";
-import {courseLessonsResolver} from "./course/course-lessons.resolver";
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {LessonsComponent} from './lessons/lessons.component';
+import {CourseComponent} from './course/course.component';
+import {courseResolver} from './course/course.resolver';
+import {courseLessonsResolver} from './course/course-lessons.resolver';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    canActivate: [isUserAuthenticated]
   },
   {
     'path': 'courses/:courseId',
     component: CourseComponent,
-    canActivate: [isUserAuthenticated],
+
     resolve: {
       course: courseResolver,
       lessons: courseLessonsResolver
     }
   },
   {
-    path: "login",
+    path: 'login',
     component: LoginComponent
   },
   {
-    path: "lessons",
+    path: 'lessons',
     component: LessonsComponent
   },
   {
